d8c6e004b1d1b9503974fd906365d0e2f2014966
wooneojun <119714067+wooneojun@users.noreply.github.com>
2023-07-19 15:04:36 +0900

    [Gold IV] Title: 타일 채우기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/2133. 타일 채우기/타일 채우기.py
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.py
@@ -1,13 +1,12 @@
-from collections import defaultdict
 import sys
 
 input = sys.stdin.readline
 
 n = int(input())
 
-dp = defaultdict(int)
+dp = [0 for _ in range(31)]
 dp[2] = 3
 for i in range(4, n + 1):
     if i % 2 == 0:
-        dp[i] = dp[i-2] * 3 + (sum(dp.values()) - dp[i-2]) * 2 + 2
+        dp[i] = dp[i-2] * 3 + (sum(dp) - dp[i-2]) * 2 + 2
 print(dp[n])
