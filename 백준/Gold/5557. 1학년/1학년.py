123be565725b2f8b9d2da145c924fdbb1cc2b53d
최원준 <119714067+wooneojun@users.noreply.github.com>
2026-02-27 09:55:14 +0900

    [Gold V] Title: 1학년, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/5557. 1학년/1학년.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+nums = list(map(int, input().split()))
+
+dp = [[0]*21 for i in range(N-1)]
+# 맨 처음꺼 넣은 값 위치에 +1
+dp[0][nums[0]] = 1
+
+for i in range(1, N-1):
+    for j in range(21):
+        if j-nums[i] >= 0: dp[i][j-nums[i]] += dp[i-1][j]
+        if j+nums[i] <= 20: dp[i][j+nums[i]] += dp[i-1][j]
+
+print(dp[-1][nums[-1]])
