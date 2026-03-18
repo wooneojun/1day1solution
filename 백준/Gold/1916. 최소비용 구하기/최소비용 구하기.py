c1bc8f53248f2ab7e5f359b821e3b76f2c0d3a69
최원준 <119714067+wooneojun@users.noreply.github.com>
2026-03-18 13:53:40 +0900

    [Gold V] Title: 최소비용 구하기, Time: 432 ms, Memory: 132980 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -5,27 +5,23 @@ input = sys.stdin.readline
 
 n = int(input())
 m = int(input())
-graph = [[] for _ in range(n+1)]
 
+graph = [[] for _ in range(n+1)]
+costs = [10**8]*(n+1)
 for _ in range(m):
-    a, b, cost = map(int, input().split())
-    graph[a].append([b, cost])
+    a, b, c = map(int, input().split())
+    graph[a].append([b, c])
 
 start, end = map(int, input().split())
-
-costs = [10**8 for _ in range(n+1)]
-heap = []
 costs[start] = 0
-heapq.heappush(heap, [0, start])
+heap = [[0, start]]
 while heap:
     cur_cost, cur_node = heapq.heappop(heap)
-    if costs[cur_node] < cur_cost:
+    if cur_cost > costs[cur_node]:
         continue
     for next_node, next_cost in graph[cur_node]:
-        sum_cost = cur_cost + next_cost
-        if sum_cost >= costs[next_node]:
-            continue
-
-        costs[next_node] = sum_cost
-        heapq.heappush(heap, [sum_cost, next_node])
+        next_num = cur_cost + next_cost
+        if next_num <costs[next_node]:
+            costs[next_node] = next_num
+            heapq.heappush(heap, [next_num, next_node])
 print(costs[end])
